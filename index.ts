030a89d3a901fda98047bd2df0a2e3ba2f369f3a
VerneriLahteenoja <verneri.lahteenoja@gmail.com>
2024-07-15 22:08:02 +0300

    refactor pinging to it's own route in preparation

--- index.ts
+++ index.ts
@@ -1,17 +1,16 @@
 import express from 'express';
 import cors from 'cors';
 
+import pingRouter from './src/routes/pingRouter'
+
 const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.use('/api/ping', pingRouter);
+
+
 const PORT = 3001;
-
-app.get('/api/ping', (_req, res) => {
-    console.log('ping');
-    res.send('pong');
-});
-
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 });
