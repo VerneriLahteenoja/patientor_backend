9e05d382fd51cc37fccea765f19de501775efa69
VerneriLahteenoja <verneri.lahteenoja@gmail.com>
2024-07-17 19:05:19 +0300

    create validators for newPatient data

---
+++ src/utils/parsers.ts
@@ -0,0 +1,51 @@
+import { Gender, NewPatient } from "../types";
+import { isString, isDate, isGender } from './validators';
+
+const parseName = (name: unknown): string => {
+  if (!name || !isString(name)) {
+    throw new Error('Incorrect or missing name');
+  }
+  return name;
+};
+const parseDate = (date: unknown): string => {
+  if (!date || !isString(date) || !isDate(date)) {
+    throw new Error('Incorrect or missing date');
+  }
+  return date;
+};
+const parseSsn = (ssn: unknown): string => {
+  // Could create a ssn specific validator if needed
+  if (!ssn || !isString(ssn)) {
+    throw new Error('Incorrect or missing ssn');
+  }
+  return ssn;
+};
+const parseGender = (gender: unknown): Gender => {
+  if (!gender || !isString(gender) || !isGender(gender)) {
+    throw new Error('Incorrect or missing gender');
+  }
+  return gender;
+};
+const parseOccupation = (occupation: unknown): string => {
+  if (!occupation || !isString(occupation)) {
+    throw new Error('Incorrect or missing occupation');
+  }
+  return occupation;
+};
+
+export const toNewPatientEntry = (object: unknown): NewPatient => {
+  if (!object || typeof object !== 'object') {
+    throw new Error('Incorrect or missing data');
+  }
+  if ('name' in object && 'dateOfBirth' in object && 'ssn' in object && 'gender' in object && 'occupation' in object) {
+    const newPatient: NewPatient = {
+      name: parseName(object.name),
+      dateOfBirth: parseDate(object.dateOfBirth),
+      ssn: parseSsn(object.ssn),
+      gender: parseGender(object.gender),
+      occupation: parseOccupation(object.occupation)
+    };
+    return newPatient;
+  }
+  throw new Error('Incorrect data: some fields are missing');
+};
