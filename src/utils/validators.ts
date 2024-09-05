ac2d6c3da545c35daa2b03f63477967df1fdb69c
VerneriLahteenoja <verneri.lahteenoja@gmail.com>
2024-09-05 21:38:10 +0300

    isNumber validator

--- src/utils/validators.ts
+++ src/utils/validators.ts
@@ -4,6 +4,10 @@ export const isString = (text: unknown): text is string => {
   return typeof text === 'string' || text instanceof String;
 };
 
+export const isNumber = (value: unknown): value is number => {
+  return typeof value === 'number' || value instanceof Number;
+}
+
 export const isDate = (date: string): boolean => {
   return Boolean(Date.parse(date));
 };
