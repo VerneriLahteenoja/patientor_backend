ce540b02d0e2f5d7393074fad9db2ac27718072f
VerneriLahteenoja <verneri.lahteenoja@gmail.com>
2024-09-09 20:24:14 +0300

    create a isObject validator

--- src/utils/validators.ts
+++ src/utils/validators.ts
@@ -6,7 +6,11 @@ export const isString = (text: unknown): text is string => {
 
 export const isNumber = (value: unknown): value is number => {
   return typeof value === 'number' || value instanceof Number;
-}
+};
+
+export const isObject = (object: unknown): object is object => {
+  return typeof object === 'object' || object instanceof Object;
+};
 
 export const isDate = (date: string): boolean => {
   return Boolean(Date.parse(date));
@@ -18,4 +22,4 @@ export const isGender = (gender: string): gender is Gender => {
 
 export const isHealthCheckRating = (rating: number): rating is HealthCheckRating => {
   return Object.values(HealthCheckRating).includes(Number(rating));
-}
+};
